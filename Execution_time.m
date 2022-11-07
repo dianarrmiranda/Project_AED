8a5a51b14daf59e2c0b7dca6e9e0de4eb0fc79ec
jnluis <jnluis@ua.pt>
2022-11-07 22:18:06 +0000

    Renaming MATLAB Variables

--- Execution_time.m
+++ Execution_time.m
@@ -1,7 +1,7 @@
 clear;clc;
 DATA = load("SolucaoProf1hour.txt");
 IF_1 = load("SolucaoProfOtimizada1hour_1IF.txt");
-IF_3 = load("SolucaoProfOtimizada_So_Seg_IF.txt");
+IF_2 = load("SolucaoProfOtimizada_So_Seg_IF.txt");
 
 n = DATA(:,1); % selecionar dos dados do .txt a primeira coluna com os valores de n
 t = DATA(:,4); % selecionar dos dados do .txt a quarta coluna com os valores de n
@@ -84,42 +84,42 @@ end
 t_log_1if =log10(t_1if);
 
 %% construir o grafico para só segundo if
-n_3if = IF_3(:,1);
-t_3if = IF_3(:,4);
+n_2if = IF_2(:,1);
+t_2if = IF_2(:,4);
 
-figure(6)
-plot(n_3if,log10(t_3if),"b")
-t_log_3if =log10(t_3if);
-N = [n_3if(20:end) 1+0*n_3if(20:end)];
-Coefs = pinv(N)*t_log_3if(20:end); % matriz de regressão
+figure(5)
+plot(n_2if,log10(t_2if),"g")
+t_log_2if =log10(t_2if);
+N = [n_2if(20:end) 1+0*n_2if(20:end)];
+Coefs = pinv(N)*t_log_2if(20:end); % matriz de regressão
 
 hold on
-Ntotal = [n_3if n_3if*0+1];
+Ntotal = [n_2if n_2if*0+1];
 % regra de ajuste aos dados
-plot(n_3if, Ntotal*Coefs, "k")
-title("Tempo de execução do algoritmo com 3 if");
+plot(n_2if, Ntotal*Coefs, "k")
+title("Tempo de execução do algoritmo com 2 if");
 xlabel("n");
 ylabel("log10 (t)")
 grid on
 hold off
-t800_log_3if = [800 1]* Coefs;
-t800_3if = 10^t800_log_3if / 3600 / 24 /365;
-fprintf("O programa só com o segundo IF iria demorar %d a executar se corresse até à posição 800.\n",t800_3if);
+t800_log_2if = [800 1]* Coefs;
+t800_2if = 10^t800_log_2if / 3600 / 24 /365;
+fprintf("O programa só com o segundo IF iria demorar %d a executar se corresse até à posição 800.\n",t800_2if);
 
 n= 1:800;
 for i=n
-    t_3if(i)= [i 1]*Coefs;
-    t_3if(i)= 10.^t_3if(i) / 3600 / 24 /365;
+    t_2if(i)= [i 1]*Coefs;
+    t_2if(i)= 10.^t_2if(i) / 3600 / 24 /365;
 end
-t_log_3if =log10(t_3if);
+t_log_2if =log10(t_2if);
 %% Todos os gráficos dos diferentes algoritmos para 800 n's 
-figure(7)
+figure(6)
 hold on
 plot(n,t_log, "k")
 plot(n,t_log_1if, "r")
-plot(n,t_log_3if, "b")
+plot(n,t_log_2if, "b")
 xlabel("n");
 ylabel("log (t)");
 title("Reta de ajuste dos diferentes algoritmos para n=800")
-legend("Original","IF do n saltos","IF das velocidades","2 IF juntos")
+legend("Original","IF do n saltos","IF das velocidades")
 grid on
