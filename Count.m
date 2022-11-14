a4f16d0d78ceee49c4ffc1314b1ed894b50c45ef
jnluis <jnluis@ua.pt>
2022-11-14 22:44:54 +0000

    Adding 2 NMEC'S to same plot and rename Count.m variables

--- Count.m
+++ Count.m
@@ -3,15 +3,15 @@ DATA = load("SolucaoProf1hour.txt");
 ForE2If= load("SolProfOtimizadaForE2If.txt");
 
 n = DATA(:,1); % selecionar dos dados do .txt a primeira coluna com os valores de n
-t = DATA(:,3); % selecionar dos dados do .txt a terceira coluna com os valores de n
+count = DATA(:,3); % selecionar dos dados do .txt a terceira coluna com os valores de n
 
 figure(1)
-stem(n,t);
+stem(n,count);
 
 
 %% construir o grafico para a 2ª melhoria: FOR mais 2 IF
 n_F2if = ForE2If(:,1);
-t_F2if = ForE2If(:,3);
+count_F2if = ForE2If(:,3);
 
 figure(2)
-bar(n_F2if,t_F2if);
+bar(n_F2if,count_F2if);
